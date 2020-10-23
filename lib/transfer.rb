26fbf4b3d2964b377fff2a5fa25b61b0ca3b88f6
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-10-23 15:25:19 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
-  # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?()
+    (@sender.valid?) && (@receiver.valid?) ? true : false
+  end
+  
+  def execute_transaction()
+    @sender.deposit(-@amount)
+    @receiver.deposit(@amount)
+    @status = "complete"
+  end
 end
